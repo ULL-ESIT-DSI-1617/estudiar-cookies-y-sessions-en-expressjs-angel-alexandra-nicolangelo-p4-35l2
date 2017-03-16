2791b737f07cb06523ddc4eceb7862cb633c6be1
Nicolangelo Famiglietti <alu0100912005@ull.edu.es>
2017-03-16 11:17:28 +0000

    añadiendo express-simple-auth

---
+++ express-simple-authentication-example/app.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var port = 8080;
+var app = express();
+
+function checkAuth (req, res, next) {
+	console.log('checkAuth ' + req.url);
+
+	// don't serve /secure to those not logged in
+	// you should add to this list, for each and every secure url
+	if (req.url === '/secure' && (!req.session || !req.session.authenticated)) {
+		res.render('unauthorised', { status: 403 });
+		return;
+	}
+	next();
+}
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+var session = require('express-session');
+app.use(session({
+    secret: 'example',
+    resave: true,
+    saveUninitialized: true
+}));
+ 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var flash = require('express-flash');
+app.use(flash());
+
+app.use(checkAuth);
+//	app.use(app.router);
+app.set('view engine', 'pug');
+app.set('view options', { layout: false });
+
+var auth = require('./controllers/auth.js');
+app.use(auth);
+
+var contents = require('./controllers/contents.js');
+app.use(contents);
+
+app.listen(port);
+console.log('Node listening on port %s', port);
